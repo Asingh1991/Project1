2e48c7f5f13d81d49b367db562461a6b103340d7
Jet <elesebatanksley@yahoo.com>
2019-04-25 20:02:55 -0500

    changed name of my html

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,34 +1,68 @@
-
 $("#food-search").on("click", function() {
     var food = $("#food-input").val().trim();
-    console.log (food)
-
-    var queryURL = "https://api.nal.usda.gov/ndb/reports/?ndbno=01009&type=f&format=json&api_key=dD7Pf7jRvGjQ4wdtk47L2KISBlUnbLbzUBVEeqkn";
-    // var queryURL = " https://api.nal.usda.gov/ndb/search/?format=json&q=" + food + "&sort=n&max=5&offset=0&api_key=dD7Pf7jRvGjQ4wdtk47L2KISBlUnbLbzUBVEeqkn";
+    var api_Key = "dD7Pf7jRvGjQ4wdtk47L2KISBlUnbLbzUBVEeqkn";
+    var nurSearchURL = " https://api.nal.usda.gov/ndb/search/?format=json&q=" + food + "&sort=n&max=5&offset=0&api_key=" + api_Key;
     $.ajax({
-        url: queryURL,
-        method: "GET"
-        })
-        .then(function(response) {
-        var results = response.data;
+method: "GET",
+dataType: "json",
+url: nurSearchURL,
+success: function (response) {
+    // console.log(response);
+    console.log(response.list.item[0].ndbno);
 
-        //   for (var i = 0; i < results.length; i++) {
-        //     var foodDiv = $("#nutrition-area");
 
-        //     var nutrients = results[i].nutrients;
+    findNutrients();
 
-        //     var p = $("<p>").text("Nutrients: " + nutrients);
 
-        //     // var personImage = $("<img>");
-        //     // personImage.attr("src", results[i].images.fixed_height.url);
+function findNutrients() {
+    var nutrientId = response.list.item[0].ndbno;
+    var nutrientsAPI = "https://api.nal.usda.gov/ndb/reports/?ndbno=" + nutrientId + "&type=f&format=json&api_key=" + api_Key;
+   
 
-        //     foodDiv.prepend(p);
-        //     // gifDiv.prepend(personImage);
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: nutrientsAPI,
+        success: function (nutrients) {
+    //         console.log(reports.food.nutrients);
+            console.log("nutrients=" + nutrients);
+          
+            var nutrientFact = nutrients.report.food.nutrients;
+            // $("#nutrition-area").append(nutrientFact);
+            console.log(nutrientFact);
 
-            // $("#nutrition-area").prepend(foodDiv);
-            console.log(response);
-        // }
+            var totalKcal = nutrientFact[0].value;
+            console.log(totalKcal);
+
+            var protein = nutrientFact[1].value;
+            console.log(protein);
+
+            var carbohydrates = nutrientFact[3].value;
+            console.log(carbohydrates);
+
+            var fat = nutrientFact[2].value;
+            console.log(fat);
+
+            var newRow = $("#nutrition-data").append(
+                $("<td>").text(totalKcal),
+                $("<td>").text(protein),
+                $("<td>").text(carbohydrates),
+                $("<td>").text(fat),
+               
+            );
+                $("#nutriton-area").append(newRow);
+            
+            
+        }
+        
     });
+
+}
+}
+    
+
+});
+
 
     var apiKey = "Z6Tnh1gToaeIZG64YkHnhog0gv2kA36Z";
     var searchURL = "https://api.bigoven.com/recipes?title_kw="+food+"&pg=1&rpp=20&api_key=" + apiKey;
--- javascript.js
+++ javascript.js
@@ -1,71 +1,71 @@
-$("#food-search").on("click", function() {
-    var food = $("#food-input").val().trim();
-    var api_Key = "dD7Pf7jRvGjQ4wdtk47L2KISBlUnbLbzUBVEeqkn";
-    var nurSearchURL = " https://api.nal.usda.gov/ndb/search/?format=json&q=" + food + "&sort=n&max=5&offset=0&api_key=" + api_Key;
-    $.ajax({
-method: "GET",
-dataType: "json",
-url: nurSearchURL,
-success: function (response) {
-    // console.log(response);
-    console.log(response.list.item[0].ndbno);
+// $("#food-search").on("click", function() {
+//     var food = $("#food-input").val().trim();
+//     var api_Key = "dD7Pf7jRvGjQ4wdtk47L2KISBlUnbLbzUBVEeqkn";
+//     var nurSearchURL = " https://api.nal.usda.gov/ndb/search/?format=json&q=" + food + "&sort=n&max=5&offset=0&api_key=" + api_Key;
+//     $.ajax({
+// method: "GET",
+// dataType: "json",
+// url: nurSearchURL,
+// success: function (response) {
+//     // console.log(response);
+//     console.log(response.list.item[0].ndbno);
 
 
-    findNutrients();
+//     findNutrients();
 
 
-function findNutrients() {
-    var nutrientId = response.list.item[0].ndbno;
-    var nutrientsAPI = "https://api.nal.usda.gov/ndb/reports/?ndbno=" + nutrientId + "&type=f&format=json&api_key=" + api_Key;
+// function findNutrients() {
+//     var nutrientId = response.list.item[0].ndbno;
+//     var nutrientsAPI = "https://api.nal.usda.gov/ndb/reports/?ndbno=" + nutrientId + "&type=f&format=json&api_key=" + api_Key;
    
 
-    $.ajax({
-        method: "GET",
-        dataType: "json",
-        url: nutrientsAPI,
-        success: function (nutrients) {
-    //         console.log(reports.food.nutrients);
-            console.log("nutrients=" + nutrients);
+//     $.ajax({
+//         method: "GET",
+//         dataType: "json",
+//         url: nutrientsAPI,
+//         success: function (nutrients) {
+//     //         console.log(reports.food.nutrients);
+//             console.log("nutrients=" + nutrients);
           
-            var nutrientFact = nutrients.report.food.nutrients;
-            // $("#nutrition-area").append(nutrientFact);
-            console.log(nutrientFact);
+//             var nutrientFact = nutrients.report.food.nutrients;
+//             // $("#nutrition-area").append(nutrientFact);
+//             console.log(nutrientFact);
 
-            var totalKcal = nutrientFact[0].value;
-            console.log(totalKcal);
+//             var totalKcal = nutrientFact[0].value;
+//             console.log(totalKcal);
 
-            var protein = nutrientFact[1].value;
-            console.log(protein);
+//             var protein = nutrientFact[1].value;
+//             console.log(protein);
 
-            var carbohydrates = nutrientFact[3].value;
-            console.log(carbohydrates);
+//             var carbohydrates = nutrientFact[3].value;
+//             console.log(carbohydrates);
 
-            var fat = nutrientFact[2].value;
-            console.log(fat);
+//             var fat = nutrientFact[2].value;
+//             console.log(fat);
 
-            var newRow = $("#nutrition-data").append(
-                $("<td>").text(totalKcal),
-                $("<td>").text(protein),
-                $("<td>").text(carbohydrates),
-                $("<td>").text(fat),
+//             var newRow = $("#nutrition-data").append(
+//                 $("<td>").text(totalKcal),
+//                 $("<td>").text(protein),
+//                 $("<td>").text(carbohydrates),
+//                 $("<td>").text(fat),
                
-            );
-                $("#nutriton-area").append(newRow);
+//             );
+//                 $("#nutriton-area").append(newRow);
             
             
-        }
+//         }
         
-    });
+//     });
 
-}
-}
-    });
+// }
+// }
+//     });
 
-})
-// }
-// console.log(food),
-// console.log(response),
-    
-// }
 // })
-// });
+// // }
+// // console.log(food),
+// // console.log(response),
+    
+// // }
+// // })
+// // });
